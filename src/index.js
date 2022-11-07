7237f3a961cf3a60ba64951bd8832fad56169b85
Airmax21 <iqbal.agyan@gmail.com>
2022-11-07 19:48:24 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import Game from './App';
+import Game from './Game';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
